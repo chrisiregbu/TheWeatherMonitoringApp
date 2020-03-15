12fe42facefeb154cad8f9410484320a43c48fdb
chrisiregbu <ciregbu@gmail.com>
2020-03-15 23:17:38 +0000

    Initial code

---
+++ src/TheWeatherMonitoringApp.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheWeatherMonitoringApp.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Weather Monitoring App!");
+        }
+    }
+}
